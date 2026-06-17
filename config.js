14760e30c266e00b1f0d09d05aa4b1ec78177e3a
horiken7 <158825647+horiken7@users.noreply.github.com>
2026-06-17 12:57:04 +0900

    Configure Google OAuth client ID

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.DAILY_BRIEFING_CONFIG = {
-  // Google Cloud Consoleで作成したOAuth 2.0 クライアントIDを入れてください。
-  // 例: "1234567890-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com"
-  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com",
+  // Google Cloud Consoleで作成したOAuth 2.0 クライアントIDです。
+  GOOGLE_CLIENT_ID: "1028346367310-h5qip1vrfr468g92eub1kp4e3bdbsl9f.apps.googleusercontent.com",
 
   // 位置情報を許可しない場合の初期表示。北九州市八幡西区の目安です。
   DEFAULT_LOCATION: {
